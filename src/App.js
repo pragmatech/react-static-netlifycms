3dd826af0858d2092dea18b88f3ba81008ec25a7
dar <islandnotes@gmail.com>
2019-01-08 22:20:41 -1000

    misc

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => (
       <nav>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
+        <Link to="/blog">Content</Link>
       </nav>
       <div className="content">
         <Routes />
